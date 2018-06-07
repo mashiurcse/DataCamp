a37393a86c989c3a2fc6d3ea1f7ef23e4a92a56e
Md Mashiur Rahman <36106328+mashiurcse@users.noreply.github.com>
2018-06-07 19:55:25 +0200

    Update 04-5-fold-cross-validation.py
    Added original code!

--- 17-supervised-learning-with-scikit-learn/02-regression/04-5-fold-cross-validation.py
+++ 17-supervised-learning-with-scikit-learn/02-regression/04-5-fold-cross-validation.py
@@ -32,3 +32,21 @@ cv_auc = cross_val_score(logreg, X, y, cv=5, scoring='roc_auc')
 
 # Print list of AUC scores
 print("AUC scores computed using 5-fold cross-validation: {}".format(cv_auc))
+
+/*
+# Import the necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg,X,y,cv=5)
+
+# Print the 5-fold cross-validation scores
+print(cv_scores)
+
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+*/
+
